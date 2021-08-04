4320a5b43660ddc4ba6c24983433757a0f1ade92
Nisanur Bulut <nisanurrunasin@gmail.com>
2021-08-04 22:30:20 +0300

    implemented Router class

---
+++ Lucius/controllers/MainController.php
@@ -0,0 +1,5 @@
+class MainController{
+    public function index(){
+        
+    }
+}
--- Lucius/index.php
+++ Lucius/index.php
@@ -1,3 +1,16 @@
 <?php 
-require_once "vendor/autoload.php";
+require_once "./vendor/autoload.php";
+use app\controllers\MainController;
+use app\Router;
+$router = new Router();
+
+$router->get('/',[MainController::class,'index']);
+$router->get('/enemies',[MainController::class,'index']);
+$router->get('/enemies/create',[MainController::class,'create']);
+$router->post('/enemies/create',[MainController::class,'create']);
+$router->get('/enemies/update',[MainController::class,'update']);
+$router->post('/enemies/update',[MainController::class,'update']);
+$router->post('/enemies/delete',[MainController::class,'delete']);
+
+$router->resolve();
 ?>
