361cfd914b804ef6f29118341a1b4082a1c2a999
Nisanur Bulut <nisanurrunasin@gmail.com>
2021-08-09 20:22:38 +0300

    implemented Router.php

--- Lucius/app/Router.php
+++ Lucius/app/Router.php
@@ -2,19 +2,31 @@
 
 namespace app;
 
-class Router {
+class Router
+{
 
-    public array $getRoutes=[];
-    public array $postRoutes=[];
+    public array $getRoutes = [];
+    public array $postRoutes = [];
 
 
-    public function get($url, $fn){
-        $this->getRoutes[$url]=$fn;
+    public function get($url, $fn)
+    {
+        $this->getRoutes[$url] = $fn;
     }
-    public function post($url, $fn){
-        $this->postRoutes[$url]=$fn;
-    }
-    public function resolve(){
 
+    public function post($url, $fn)
+    {
+        $this->postRoutes[$url] = $fn;
+    }
+
+    public function resolve()
+    {
+        $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method === 'GET') {
+            $fn = getRoutes[$currentUrl] ?? null;
+        } else if ($method === 'POST') {
+            $fn = $this->postRoutes[$currentUrl] ?? null;
+        }
     }
 }
