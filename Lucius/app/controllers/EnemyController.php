fb9c851fac0ae2b8191c71a9d298a0a902258e45
Nisanur Bulut <nisanurrunasin@gmail.com>
2021-08-10 19:33:49 +0300

    complated EnemyController.php

--- Lucius/app/controllers/EnemyController.php
+++ Lucius/app/controllers/EnemyController.php
@@ -3,14 +3,78 @@
 namespace app\controllers;
 
 use app\Router;
-
 class EnemyController
 {
     public function index(Router $router)
     {
-        $enemies = $router->db->getEnemies();
+        $keyword = $_GET['search'] ?? '';
+        $enemies = $router->database->getEnemys($keyword);
         $router->renderView('enemies/index', [
-            'enemies' => $enemies
+            'enemies' => $enemies,
+            'keyword' => $keyword
         ]);
+    }
+
+    public function create(Router $router)
+    {
+        $enemyData = [
+            'image' => ''
+        ];
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $enemyData['title'] = $_POST['title'];
+            $enemyData['description'] = $_POST['description'];
+            $enemyData['price'] = $_POST['price'];
+            $enemyData['imageFile'] = $_FILES['image'] ?? null;
+
+            $enemy = new Enemy();
+            $enemy->load($enemyData);
+            $enemy->save();
+            header('Location: /enemies');
+            exit;
+        }
+        $router->renderView('enemies/create', [
+            'enemy' => $enemyData
+        ]);
+    }
+
+    public function update(Router $router)
+    {
+        $id = $_GET['id'] ?? null;
+        if (!$id) {
+            header('Location: /enemies');
+            exit;
+        }
+        $enemyData = $router->database->getEnemyById($id);
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $enemyData['title'] = $_POST['title'];
+            $enemyData['description'] = $_POST['description'];
+            $enemyData['price'] = $_POST['price'];
+            $enemyData['imageFile'] = $_FILES['image'] ?? null;
+
+            $enemy = new Enemy();
+            $enemy->load($enemyData);
+            $enemy->save();
+            header('Location: /enemies');
+            exit;
+        }
+
+        $router->renderView('enemies/update', [
+            'enemy' => $enemyData
+        ]);
+    }
+
+    public function delete(Router $router)
+    {
+        $id = $_POST['id'] ?? null;
+        if (!$id) {
+            header('Location: /enemies');
+            exit;
+        }
+
+        if ($router->database->deleteEnemy($id)) {
+            header('Location: /enemies');
+            exit;
+        }
     }
 }
