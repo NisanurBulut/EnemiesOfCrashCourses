565a71db8f2c0214d0a6478bfe1719217b24f25c
Nisanur Bulut <nisanurrunasin@gmail.com>
2021-08-10 20:45:48 +0300

    created UtilHelper class

---
+++ Lucius/app/helpers/UtilHelper.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace app\helpers;
+
+
+class UtilHelper
+{
+    public static function randomString($n)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $str = '';
+        for ($i = 0; $i < $n; $i++) {
+            $index = rand(0, strlen($characters) - 1);
+            $str .= $characters[$index];
+        }
+
+        return $str;
+    }
+}
