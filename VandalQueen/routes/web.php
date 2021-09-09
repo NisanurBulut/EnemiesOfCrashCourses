3dddaa22e3a132358dcd9e5459c830122b430d7a
Nisanur Bulut <nisanurrunasin@gmail.com>
2021-09-09 16:43:03 +0300

    created register and dashboard controllers

--- VandalQueen/routes/web.php
+++ VandalQueen/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,8 +14,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register', [RegisterController::class, 'store']);
 
 Route::get('/posts', function () {
 return view('posts.index');
