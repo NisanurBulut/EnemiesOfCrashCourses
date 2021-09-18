02aeda787c8d75f7a96b89ed14596b8e8ffd32b7
Nisanur Bulut <nisanurrunasin@gmail.com>
2021-09-18 22:31:01 +0300

    set 5 offset

--- VandalQueen/app/Http/Controllers/PostController.php
+++ VandalQueen/app/Http/Controllers/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
 
     public function index()
     {
-        $posts = Post::latest()->with(['user', 'likes'])->paginate(20);
+        $posts = Post::latest()->with(['user', 'likes'])->paginate(5);
 
         return view('posts.index', [
             'posts' => $posts
