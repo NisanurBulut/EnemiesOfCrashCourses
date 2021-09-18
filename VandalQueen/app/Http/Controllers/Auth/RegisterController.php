ed1e0e8fc0c6fcd38536d5d85df5db1ec432c88d
Nisanur Bulut <nisanurrunasin@gmail.com>
2021-09-18 22:33:45 +0300

    fixed route after register

--- VandalQueen/app/Http/Controllers/Auth/RegisterController.php
+++ VandalQueen/app/Http/Controllers/Auth/RegisterController.php
@@ -30,6 +30,6 @@ class RegisterController extends Controller
           'password'=>Hash::make($request->password)
       ]);
       auth()->attempt($request->only('email','password'));
-      return redirect()->route('dashboard');
+      return redirect()->route('posts.index');
     }
 }
