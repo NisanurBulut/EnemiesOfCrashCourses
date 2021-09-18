a5d563ff6ea9d1067582ee13e697c92f32b8bbe0
Nisanur Bulut <nisanurrunasin@gmail.com>
2021-09-18 19:45:45 +0300

    implemented post policy

--- VandalQueen/app/Http/Controllers/PostController.php
+++ VandalQueen/app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::with(['user', 'likes'])->paginate(5);
+        $posts = Post::latest()->with(['user', 'likes'])->paginate(5);
         return view('posts.index', [
             "posts" => $posts
         ]);
@@ -24,6 +24,7 @@ class PostController extends Controller
         return back();
     }
     public function destroy(Post $post) {
+        $this->authorize('delete', $post);
         $post->delete();
         return back();
     }
--- VandalQueen/app/Models/Post.php
+++ VandalQueen/app/Models/Post.php
@@ -12,10 +12,7 @@ class Post extends Model
     protected $fillable = [
         'body'
     ];
-    public function ownedBy(User $user)
-    {
-        return $this->user_id == $user->id;
-    }
+
     public function likedBy(User $user)
     {
         return $this->likes->contains('user_id', $user->id);
