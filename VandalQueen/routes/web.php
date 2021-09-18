3b65b0d8181e4b3cf4f2ccaa855d010f6d9180d9
Nisanur Bulut <nisanurrunasin@gmail.com>
2021-09-18 18:47:24 +0300

    implemented like and unlike methods

--- VandalQueen/routes/web.php
+++ VandalQueen/routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\auth\LoginController;
 use App\Http\Controllers\auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\PostLikeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,3 +35,7 @@ Route::post('/login', [LoginController::class, 'store']);
 
 Route::get('/posts', [PostController::class, 'index'])->name('posts');
 Route::post('/posts', [PostController::class, 'store']);
+
+Route::post('/posts/{post}/likes', [PostLikeController::class,'store'])->name('posts.likes');
+Route::delete('/posts/{post}/likes', [PostLikeController::class,'destroy'])->name('posts.likes');
+
