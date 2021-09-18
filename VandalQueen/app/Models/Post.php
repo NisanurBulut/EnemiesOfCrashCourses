5ed60cf148801a1d73c7d5642f548c6f7491429a
Nisanur Bulut <nisanurrunasin@gmail.com>
2021-09-18 19:03:02 +0300

    implemented ownedby method

--- VandalQueen/app/Models/Post.php
+++ VandalQueen/app/Models/Post.php
@@ -12,6 +12,10 @@ class Post extends Model
     protected $fillable = [
         'body'
     ];
+    public function ownedBy(User $user)
+    {
+        return $this->user_id == $user->id;
+    }
     public function likedBy(User $user)
     {
         return $this->likes->contains('user_id', $user->id);
