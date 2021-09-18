74662d3d69a372d4274a493b8e077c4e5bf4f159
Nisanur Bulut <nisanurrunasin@gmail.com>
2021-09-18 18:18:00 +0300

    migrated like model

--- VandalQueen/app/Models/Post.php
+++ VandalQueen/app/Models/Post.php
@@ -16,4 +16,8 @@ class Post extends Model
     {
         return $this->belongsTo(User::class);
     }
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
 }
--- VandalQueen/app/Models/User.php
+++ VandalQueen/app/Models/User.php
@@ -46,4 +46,7 @@ class User extends Authenticatable
     {
         return $this->hasMany(Post::class);
     }
+    public function likes(){
+        return $this->hasMany(Like::class);
+    }
 }
