414d3a4311b992885cf443536096f9f16c1f561d
Nisanur Bulut <nisanurrunasin@gmail.com>
2021-09-18 20:08:00 +0300

    implemented blade component

--- VandalQueen/app/Models/User.php
+++ VandalQueen/app/Models/User.php
@@ -49,4 +49,7 @@ class User extends Authenticatable
     public function likes(){
         return $this->hasMany(Like::class);
     }
+    public function receivedLikes(){
+        return $this->hasManyThrough(Like::class, Post::class);
+    }
 }
