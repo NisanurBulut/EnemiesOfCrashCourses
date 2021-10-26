ba82718344f0c116b3fe4e1ffa611b6c68861db1
Nisanur Bulut <nisanurrunasin@gmail.com>
2021-10-26 15:40:23 +0300

    implemented posts.blade

--- Gosha/resources/views/posts.blade.php
+++ Gosha/resources/views/posts.blade.php
@@ -3,14 +3,8 @@
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         <x-post-featured-card :post="$posts[0]"></x-post-featured-card>
         <div class="lg:grid lg:grid-cols-2">
-            <x-post-card></x-post-card>
-            <x-post-card></x-post-card>
-        </div>
-
-        <div class="lg:grid lg:grid-cols-3">
-            <x-post-card></x-post-card>
-            <x-post-card></x-post-card>
-            <x-post-card></x-post-card>
+            @foreach($posts->skip(1) as $post){
+            <x-post-card :post="$post"></x-post-card>
         </div>
     </main>
 </x-layout>
