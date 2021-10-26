203db8a9d07db4190c0bd92463a5f8e65e13757e
Nisanur Bulut <nisanurrunasin@gmail.com>
2021-10-26 15:58:11 +0300

    implemented posts.blade

--- Gosha/resources/views/posts.blade.php
+++ Gosha/resources/views/posts.blade.php
@@ -1,10 +1,15 @@
 <x-layout>
     @include('_posts-header')
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if($posts->count()){
         <x-post-featured-card :post="$posts[0]"></x-post-featured-card>
         <div class="lg:grid lg:grid-cols-2">
             @foreach($posts->skip(1) as $post){
             <x-post-card :post="$post"></x-post-card>
         </div>
+        @else {
+        <p>No post data</p>
+        }
+
     </main>
 </x-layout>
